3644bc31ab4d2cdac7756480029913546cc69498
Yusufkhan-hub <57767821+Yusufkhan-hub@users.noreply.github.com>
2020-09-23 13:10:44 +0530

    done it again

--- gym_site/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import signUp,feedback
-# Register your models here.
-admin.site.register(signUp)
-admin.site.register(feedback)
--- gym_site/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GymSiteConfig(AppConfig):
-    name = 'gym_site'
